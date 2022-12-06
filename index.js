79e9c3b5b052fdcf1ffbcdcf6238ed6aedcbd39d
Ananda Fidelis <acfs.developer@gmail.com>
2022-12-06 06:53:38 -0300

    Atividade 2 de JS

---
+++ index.js
@@ -0,0 +1,7 @@
+//Exemplo de código usando JavaScript
+var x = 5; //Brasil é Penta
+var y = 1; //Brasil ganhando a copa de 2022
+var z = x + y; //Brasil virando Hexacampeão
+console.log('Se o Brasil ganhar a copa, terá:',z,'estrelas')
+
+//Escolhi o Visual Studio Code para programar na linguagem JavaScript porque é uma IDE que já tenho alguma familiaridade.
